321721725a4a93e981afb8573d608d3abb46dfa5
João Vitor dos Santos Alves <joao.vitor111002@gmail.com>
2023-10-23 16:58:55 -0300

    finalizando o projeto

--- script.js
+++ script.js
@@ -5,6 +5,28 @@ const longoBt = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector('.app__image')
 const titulo = document.querySelector('.app__title')
 const botoes = document.querySelectorAll('.app__card-button')
+const startPauseBt = document.querySelector('#start-pause')
+const musicaFocoInput = document.querySelector('#alternar-musica')
+const iniciarOuPausarBt = document.querySelector('#start-pause span')
+const iconeBt = document.querySelector('.app__card-primary-butto-icon')
+const tempoNaTela = document.querySelector('#timer')
+
+const musica = new Audio('/sons/luna-rise-part-one.mp3')
+const audioPause = new Audio('/sons/pause.mp3')
+const audioPlay = new Audio('/sons/play.wav')
+const audioTempoFinalizado = new Audio('/sons/beep.mp3')
+let tempoDecorridoEmSegundos = 1500
+let intervaloId = null
+musica.loop = true
+
+
+musicaFocoInput.addEventListener('change', ()=> {
+    if (musica.paused) {
+        musica.play()
+    } else {
+        musica.pause()
+    }
+})
 
 focoBt.addEventListener('click', () => {
     alterarContexto('foco')
@@ -37,7 +59,49 @@ function alterarContexto(contexto) {
             break;
         case "descanso-longo":
             titulo.innerHTML = `Hora de voltar à superfície.<strong class="app__title-strong"> Faça uma pausa longa.</strong>`
+            break
         default:
             break;
     }
 }
+
+const contagemRegressiva = () => {
+    if(tempoDecorridoEmSegundos <= 0) {
+        audioTempoFinalizado.play()
+        alert('Tempo Finalizado')
+        zerar()
+        return
+    }
+    tempoDecorridoEmSegundos -= 1
+    mostrarTempo()
+}
+
+startPauseBt.addEventListener('click', iniciarOuPausar)
+
+function iniciarOuPausar() {
+    if(intervaloId) {
+        zerar()
+        audioPause.play()
+        return
+    }
+    audioPlay.play()
+    iconeBt.setAttribute('src', '/imagens/pause.png')
+    intervaloId = setInterval(contagemRegressiva, 1000) //2 parametros
+    iniciarOuPausarBt.textContent = "Pausar"
+}
+
+function zerar() {
+    clearInterval(intervaloId)
+    iniciarOuPausarBt.textContent = "Começar"
+    iconeBt.setAttribute('src', '/imagens/play_arrow.png')
+    intervaloId = null
+}
+
+function mostrarTempo() {
+    const tempo = new Date(tempoDecorridoEmSegundos * 1000)
+    const tempoFormatado = tempo.toLocaleDateString('pt-br', {minute: '2-digit', second: '2-digit'})
+    tempoNaTela.innerHTML = `${tempoFormatado}`
+
+}
+
+mostrarTempo()
