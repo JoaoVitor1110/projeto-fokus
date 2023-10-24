d7b1c3c9821d5754bf1ab0146f503706bb77b31f
João Vitor dos Santos Alves <joao.vitor111002@gmail.com>
2023-10-23 21:21:34 -0300

    projeto final

--- script.js
+++ script.js
@@ -29,21 +29,25 @@ musicaFocoInput.addEventListener('change', ()=> {
 })
 
 focoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 1500
     alterarContexto('foco')
     focoBt.classList.add('active')
 })
 
 curtoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 300
     alterarContexto('descanso-curto')
     curtoBt.classList.add('active')
 })
 
 longoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 900
     alterarContexto('descanso-longo')
     longoBt.classList.add('active')
 })
 
 function alterarContexto(contexto) {
+    mostrarTempo()
     botoes.forEach(function (contexto) {
         contexto.classList.remove('active')
     })
