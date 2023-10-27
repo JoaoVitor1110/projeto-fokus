9b246a35bfb040017dc152f8d2ce1be85291fdbf
João Vitor dos Santos Alves <joao.vitor111002@gmail.com>
2023-10-27 15:14:25 -0300

    ;

--- script-crud.js
+++ script-crud.js
@@ -9,55 +9,62 @@ const textArea = document.querySelector('.app__form-textarea')
 
 let tarefas = []
 
-const taskIconSvg = `
-<svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24"
+const taskIconSvg = `<svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24"
     fill="none" xmlns="http://www.w3.org/2000/svg">
     <circle cx="12" cy="12" r="12" fill="#FFF" />
     <path
         d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z"
         fill="#01080E" />
-</svg>
-`
+</svg>`
+
+const limparForm = () => {
+    textArea.value = '';
+    formTask.classList.add('hidden');
+}
+
 function createTask(tarefas) {
-    const li = document.createElement('li')
-    li.classList.add('app__section-task-list-item')
+    const li = document.createElement('li');
+    li.classList.add('app__section-task-list-item');
 
-    const svgIcon = document.createElement('svg')
-    svgIcon.innerHTML = taskIconSvg
+    const svgIcon = document.createElement('svg');
+    svgIcon.innerHTML = taskIconSvg;
 
-    const paragraph = document.createElement('p')
-    paragraph.classList.add('app__section-task-list-description')
+    const paragraph = document.createElement('p');
+    paragraph.classList.add('app__section-task-list-description');
 
-    paragraph.textContent = tarefas.descricao
+    paragraph.textContent = tarefas.descricao;
 
-    li.appendChild(svgIcon)
-    li.appendChild(paragraph)
+    li.appendChild(svgIcon);
+    li.appendChild(paragraph);
 
-    return li
+    return li;
 }
 
 tarefas.forEach(task => {
-    const taskItem = createTask(task)
-    taskListContainer.appendChild(taskItem)
+    const taskItem = createTask(task);
+    taskListContainer.appendChild(taskItem);
 })
     
 toggleFormTaskBtn.addEventListener('click', () => {
-    formLabel.textContent = 'Adicionando Tarefa'
-    formTask.classList.toggle('hidden')
+    formLabel.textContent = 'Adicionando Tarefa';
+    formTask.classList.toggle('hidden');
 })
 
 cancelFormTaskBtn.addEventListener('click', ()=> {
-    formTask.classList.toggle('hidden')
-    textArea.value = ""
+    formTask.classList.toggle('hidden');
+    textArea.value = "";
+    limparForm();
 })
 
 formTask.addEventListener('submit', (evento) => {
-    evento.preventDefault()
+    evento.preventDefault();
     const task = {
         descricao: textArea.value,
         concluida: false
     }
-    tarefas.push(task)
-    const taskItem = createTask(task)
-    taskListContainer.appendChild(taskItem)
+    tarefas.push(task);
+    const taskItem = createTask(task);
+    taskListContainer.appendChild(taskItem);
+    limparForm();
 })
+
